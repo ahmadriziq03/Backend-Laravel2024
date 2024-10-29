fa8701ddb4b8401d3585222327053845a08bfee9
ahmadriziq03 <ahmadriziq010@gmail.com>
2024-10-29 09:42:53 +0700

    Tugas Pertemuan 5

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+
+        $data = [
+            'message' => 'Get All Student',
+            'data'=> $students
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan,
+        ];
+
+        $student = Student::create($input);
+
+        $data = [
+            'message' => 'Student is created successfully',
+            'data' => $student
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+
+        $student->update([
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan,
+        ]);
+
+        return response()->json([
+            'message' => 'Student is updated successfully',
+            'data' => $student
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+
+        $student->delete();
+
+        return response()->json([
+            'message' => 'Student is deleted successfully'
+        ], 200);
+    }
+}
--- database/migrations/2024_10_22_124407_create_personal_access_tokens_table.php
+++ database/migrations/2024_10_22_124407_create_personal_access_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->morphs(name: 'tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
-    }
+    } 
 
     /**
      * Reverse the migrations.
@@ -31,3 +31,10 @@ return new class extends Migration
         Schema::dropIfExists('personal_access_tokens');
     }
 };
+
+
+
+
+
+
+
